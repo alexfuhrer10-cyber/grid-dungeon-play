41aaf14f2c5b06872916b8a5ee577298f653f022
Alex Fuhrer <alexfuhrer10@gmail.com>
2026-07-29 23:11:47 -0700

    kkd-84: Notebook + Pages redesign

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* KenKen Dungeon service worker.
    Navigations are network-first so new builds always arrive; hashed assets and art
    are cache-first for instant loads and offline play. */
-const CACHE = "kkd-83";
+const CACHE = "kkd-84";
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest"])).then(() => self.skipWaiting()));
 });
